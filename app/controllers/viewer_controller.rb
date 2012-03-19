218a4d798f288a246bb4c5115312a4af7eeb3ff0
omar gallo <omargallob@gmail.com>
2012-03-19 13:59:45 +0100

    hompage posts

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,5 +1,8 @@
 class ViewerController < ApplicationController
   def show
     @page = Page.find_by_name(params[:name].gsub("_"," ")||"home")    
+    if @page.name == "home"
+    	@posts = Post.all
+    end
   end
 end

090d3d9c67da4f9b9a048919a503ac423f327416
omar gallo <omargallob@gmail.com>
2012-03-20 17:37:35 +0100

    fix back to list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def show
   	@post = Post.find(params[:id])
 
-   	set_meta_tags :title => @post.title
+   	set_meta_tags :title => "#{@post.category.title} | "+ @post.title
   	render :layout => "show"
 
   end

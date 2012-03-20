3e5322f570897f4611269bc4f5c767f835d5101f
omar gallo <omargallob@gmail.com>
2012-03-20 17:50:34 +0100

    gallery controller

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -11,7 +11,7 @@ class GalleriesController < ApplicationController
 
   def show
   	@album = Album.find_by_id(params[:id])
-		set_meta_tags :title => "Gallery | #{@album.title}",
-  								:description => @album.description
+		# set_meta_tags :title => "Gallery | #{@album.title}",
+  # 								:description => @album.description
   end
 end

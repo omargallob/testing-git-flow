a9f2e086e71eb8e2fb7cf35320c5d92735ad4281
omar gallo <omargallob@gmail.com>
2012-03-21 12:50:04 +0100

    about fix

--- app/assets/javascripts/customRules.js
+++ app/assets/javascripts/customRules.js
@@ -1,20 +1,7 @@
 $(function() {
 	//alert("fuck yeah!!");
 	
-	$("img#logo").hover(
-		function () {
-	    $(this).attr("src","/assets/CMS_logo_over.png");
-	  }, 
-	  function () {
-			$(this).attr("src","/assets/CMS_logo.png");
-	  });
-		$("img#flogo").hover(
-			function () {
-		    $(this).attr("src","/assets/logo_over.png");
-		  }, 
-		  function () {
-				$(this).attr("src","/assets/logo.png");
-		  });
+
 
 	$("li#facebook a").attr("target","blank");
 

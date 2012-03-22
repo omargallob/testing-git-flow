f2b3cfdd940a0c3cc5d1031d367dea5395337098
omar gallo <omargallob@gmail.com>
2012-03-22 20:31:45 +0100

    css for v2 - switch white and grey

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require colorbox/colorbox
+//= require validation/validationEngine
+//= require validation/languages/validationEngine-en
+//= require masonry/jquery.masonry.min
+//= require ckeditor/init
+//= require customRules.js
